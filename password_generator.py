7be11faf462c45519ca21a6d8096cbdae16116c9
Zaid Ahmed <zaid.arain.o11@gmail.com>
2026-07-12 06:49:48 +0500

    Refactor password generation into a class structure

--- password_generator.py
+++ password_generator.py
@@ -1,67 +1,79 @@
 import random
 
+class PasswordGenerator:
+    def __init__(self):
+        self.uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.lowercase = "abcdefghijklmnopqrstuvwxyz"
+        self.numbers = "0123456789"
+        self.characters = "!@#$%^&*()_+-=[]{}|;:,.<>?"
+
+    def build_pool(self, upper, lower, numbers, characters):
+        pool = ""
+
+        if upper.lower() == "y":
+            pool += self.uppercase
+
+        if lower.lower() == "y":
+            pool += self.lowercase
+
+        if numbers.lower() == "y":
+            pool += self.numbers
+
+        if characters.lower() == "y":
+            pool += self.characters
+
+        return pool
+
+    def generate_password(self, length, pool):
+        password = ""
+
+        for i in range(length):
+            password += random.choice(pool)
+
+        return password
+
+
 def menu():
-    print("""1. GENERATE PASSWORD
-0. EXIT""")
+    print("""
+1. GENERATE PASSWORD
+0. EXIT
+""")
 
-def password_generator(length, pool):
-    password = ""
 
-    for i in range(length):
-        password += random.choice(pool)
+generator = PasswordGenerator()
 
-    return password
-    
 while True:
 
-    print("\n~~~PASSWORD GENERATOR~~~\n")
+    print("\n~~~ PASSWORD GENERATOR ~~~\n")
     menu()
+
     choice = input("Enter Choice: ")
+
     if choice == "0":
         print("Goodbye!")
         break
-    if choice != "1":
-        print("invalid choice.")
+
+    elif choice != "1":
+        print("Invalid choice.")
         continue
 
-    UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
-    NUMBERS = "0123456789"
-    CHARACTERS = "!@#$%^&*()_+-=[]{}|;:,.<>?"
-    
-    length = int(input("enter password length: "))
+    length = int(input("Enter password length: "))
 
     if length <= 0:
-        print("password length must be greater than 0.")
+        print("Password length must be greater than 0.")
         continue
 
-    pool = ""
+    upper = input("Include uppercase letters? (y/n): ")
+    lower = input("Include lowercase letters? (y/n): ")
+    numbers = input("Include numbers? (y/n): ")
+    characters = input("Include special characters? (y/n): ")
 
-    upper = input("include uppercase letters? (y/n): ")
-    if upper.lower() == "y":
-        pool += UPPERCASE
-
-    lower = input("include lowercase letters? (y/n):")
-    if lower.lower() == "y":
-        pool += LOWERCASE
-
-    numbers = input("include numbers? (y/n): ")
-    if numbers.lower() == "y":
-        pool += NUMBERS
-
-    characters = input("include special characters? (y/n): ")
-    if characters.lower() == "y":
-        pool += CHARACTERS
+    pool = generator.build_pool(upper, lower, numbers, characters)
 
     if pool == "":
-        print("you must select at least one character type.")
+        print("You must select at least one character type.")
         continue
 
-    password = password_generator(length, pool)
+    password = generator.generate_password(length, pool)
 
-    print(f"Generated Password: {password}")
-
-    again = input("generate another password? (y/n): ")
-    if again.lower() == "n":
-        print("Goodbye!")
-        break
+    print(f"\nGenerated Password: {password}")
